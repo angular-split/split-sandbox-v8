39bc9bee1ab38e934af6a3024cab1e6a928186dc
Bram Borggreve <borggreve@gmail.com>
2020-07-23 18:23:13 -0500

    feat: add demo for angular-split on Angular 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'split-sandbox-v8';
+  version = VERSION.full;
 }
